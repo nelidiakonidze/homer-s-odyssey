f2895104479a1d082ba3d745bb9425788d56f5a3
Neli <diakonidzeneli@gmail.com>
2019-06-04 18:50:56 +0200

    deploy netlifly

---
+++ front/React/src/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type='email' name='email' />
+      </div>
+    );
+  }
+}
+
+export default SignUp;
