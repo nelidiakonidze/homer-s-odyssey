d4169e0ac90118b67932fa4ed8dbb6a3c2a15d2a
Neli <diakonidzeneli@gmail.com>
2019-06-18 17:59:37 +0200

    orgnaizing folders

---
+++ front/src/SignUp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = email;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <input type='email' name='email' />
+        <input type='text' name='email' />
+        <input type='text' name='email' />
+        <input type='text' name='email' />
+        <input type='text' name='email' />
+        <input type='text' name='email' />
+        <input type='submit' value='Submit' />
+      </div>
+    );
+  }
+}
