a3948ee022e9b610c23ec695e31ca45c1eada07c
Neli <diakonidzeneli@gmail.com>
2019-07-02 12:25:07 +0200

    done the 030 quest

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,19 +1,68 @@
 import React from 'react';
 import './App.css';
 
-function SignUp() {
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <input type='email' name='email' />
-      <input type='text' name='email' />
-      <input type='text' name='email' />
-      <input type='text' name='email' />
-      <input type='text' name='email' />
-      <input type='text' name='email' />
-      <input type='submit' value='Submit' />
-    </div>
-  );
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: '',
+      lastName: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert('Your form was submitted!');
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  handleChange(e) {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  render() {
+    return (
+      <div className='form'>
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>email</label>
+          <input
+            type='email'
+            value={this.state.value}
+            name='email'
+            onChange={this.handleChange}
+          />
+          <label>password</label>
+          <input
+            type='text'
+            value={this.state.value}
+            name='password'
+            onChange={this.handleChange}
+          />
+          <label>name</label>
+          <input
+            type='text'
+            value={this.state.value}
+            name='name'
+            onChange={this.handleChange}
+          />
+          <label>lastName</label>
+          <input
+            type='text'
+            value={this.state.value}
+            name='lastName'
+            onChange={this.handleChange}
+          />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
