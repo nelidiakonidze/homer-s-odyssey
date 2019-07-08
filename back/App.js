e086ee2a5b247fb721b37449aa3cc43ad7807f10
Neli <diakonidzeneli@gmail.com>
2019-07-08 22:26:34 +0200

    post added

--- back/App.js
+++ back/App.js
@@ -6,12 +6,12 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
-app.use('/auth', authRouter);
-
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+
+app.use('/auth', authRouter);
 
 app.get('/', (req, res) => {
   res.send('youhou');
--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const connection = require('../../helpers/db');
+
 router.get('/', function(req, res) {
   res.send('Done');
 });
@@ -9,7 +11,17 @@ router.get('/users', function(req, res) {
 });
 
 router.post('/signup', function(req, res) {
-  res.send('I am in POST signup');
+  console.log(req.body);
+  const formData = req.body;
+  connection.query('INSERT INTO `users` SET ?', formData, (err, results) => {
+    if (err) {
+      // If an error has occurred, then the user is informed of the error
+      console.log(err);
+      res.status(500).send('Error saving a user');
+    } else {
+      // If everything went well, we send a status "ok".
+      res.sendStatus(200);
+    }
+  });
 });
-
 module.exports = router;
