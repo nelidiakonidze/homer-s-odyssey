9c39abd9d1a7f2cae8a83e5fb49ff6ff0784d564
Neli <diakonidzeneli@gmail.com>
2019-07-10 09:53:42 +0200

    quest 060

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import SignUp from './SignUp';
+import './index.css';
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <MuiThemeProvider>
+      <Grid container alignItems='center' style={{height: '100%'}}>
+        <Grid item xs={12}>
+          <Paper elevation={4} style={{margin: 90}}>
+            <Grid container alignItems='center' justify='center'>
+              <Grid item xs={12} sm={6} style={{'text-align': 'center'}}>
+                <img src='http://images4.fanpop.com/image/photos/24400000/Perry-the-platypus-perry-the-platypus-24402589-180-217.png' />
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                alignContent='center'
+                style={{padding: '3rem'}}>
+                <SignUp />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </MuiThemeProvider>
   );
 }
 
--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import {TextField, Button, Snackbar, Icon} from '@material-ui/core';
+import './SignUp.css';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -10,10 +11,12 @@ class SignUp extends React.Component {
       name: '',
       lastname: '',
       flash: '',
+      open: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   handleChange(e) {
@@ -21,60 +24,117 @@ class SignUp extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('Your form was submitted!');
+    // alert('Your form was submitted!');
     event.preventDefault();
     console.log(this.state);
-    
+    this.setState({open: true});
+
+    const {email, password, name, lastname} = this.state;
+
+    // Validate fields before sending to BE.
+
+    const payload = {
+      email,
+      password,
+      name,
+      lastname,
+    };
+
     fetch('/auth/signup', {
       method: 'POST',
       headers: new Headers({
         'Content-Type': 'application/json',
       }),
-      body: JSON.stringify(this.state),
+      body: JSON.stringify(payload),
     })
       .then(res => res.json())
       .then(
-        res => this.setState({flash: res.flash}),
+        res =>
+          this.setState({
+            flash: res.flash,
+            email: '',
+            password: '',
+            name: '',
+            lastname: '',
+          }),
         error => this.setState({flash: error.flash}),
       );
   }
 
+  handleClose() {
+    this.setState({open: false});
+  }
+
   render() {
     return (
-      <div className='form'>
-        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>email</label>
-          <input
-            type='email'
-            value={this.state.email}
-            name='email'
-            onChange={this.handleChange}
+      <React.Fragment>
+        <div>
+          <h1>Sign up!</h1>
+          <form
+            onSubmit={this.handleSubmit}
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              flexDirection: 'column',
+              margin: '10px',
+            }}>
+            <label>Email</label>
+            <TextField
+              type='email'
+              value={this.state.email}
+              name='email'
+              onChange={this.handleChange}
+            />
+            <label>Password</label>
+            <TextField
+              type='text'
+              value={this.state.password}
+              name='password'
+              onChange={this.handleChange}
+            />
+            <label>Name</label>
+            <TextField
+              type='text'
+              value={this.state.name}
+              name='name'
+              onChange={this.handleChange}
+            />
+            <label>Last name</label>
+            <TextField
+              type='text'
+              value={this.state.lastname}
+              name='lastname'
+              onChange={this.handleChange}
+            />
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              component='span'
+              onClick={this.handleSubmit}
+              style={{
+                margin: '5px',
+                display: 'flex',
+                alignSelf: 'flex-end',
+              }}>
+              Sign up
+            </Button>
+          </form>
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            open={this.state.open}
+            autoHideDuration={2000}
+            onClose={this.handleClose}
+            ContentProps={{
+              'aria-describedby': 'message-id',
+            }}
+            message={<span id='message-id'>{this.state.flash}</span>}
           />
-          <label>password</label>
-          <input
-            type='text'
-            value={this.state.password}
-            name='password'
-            onChange={this.handleChange}
-          />
-          <label>name</label>
-          <input
-            type='text'
-            value={this.state.name}
-            name='name'
-            onChange={this.handleChange}
-          />
-          <label>lastName</label>
-          <input
-            type='text'
-            value={this.state.lastname}
-            name='lastname'
-            onChange={this.handleChange}
-          />
-          <input type='submit' value='Submit' />
-        </form>
-      </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
